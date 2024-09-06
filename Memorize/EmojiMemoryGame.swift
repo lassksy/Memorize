9f84fa52f4d0a8403f1a3df7e5895868987d7249
kurokoo <80524293+lassksy@users.noreply.github.com>
2024-09-06 17:13:23 +0800

    lecture 5

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -14,7 +14,7 @@ class EmojiMemoryGame : ObservableObject {
     
     private static func createMemoryGame() -> MemoryGame<String> {
         return MemoryGame(
-            numberOfPairsOfCards: 16 ) { pairIndex in
+            numberOfPairsOfCards: 10 ) { pairIndex in
                 if emojis.indices.contains(pairIndex) {
                     return emojis[pairIndex]
                 } else {
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -14,6 +14,7 @@ struct EmojiMemoryGameView: View {
         VStack {
             ScrollView {
                 card
+                    .animation(.default, value: viewModel.cards)
             }
             Button("Shuffle") {
                 viewModel.shuffle()
@@ -25,13 +26,16 @@ struct EmojiMemoryGameView: View {
     
     var card: some View {
         LazyVGrid(columns:[GridItem(.adaptive(minimum: 85), spacing: 0)] , spacing: 0) {
-            ForEach(viewModel.cards.indices, id:\.self) { index in
-                CardView(viewModel.cards[index])
-                    .aspectRatio(2/3 , contentMode: .fit)
-                    .foregroundColor(Color.blue)
-                    .padding(4)
-                
-            }
+            ForEach(viewModel.cards) { card in
+                    
+                    CardView(card)
+                        .aspectRatio(2/3 , contentMode: .fit)
+                        .foregroundColor(Color.blue)
+                        .padding(4)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
+                }
         }
         
     }
@@ -61,7 +65,7 @@ struct EmojiMemoryGameView: View {
                 base.fill()
                     .opacity(card.isFaceUp ? 0 : 1)
                 }
-          
+            .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
             }
         }
     }
